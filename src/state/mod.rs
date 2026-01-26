39cf3eb17d2cca3d64cd7a6847a4ad6aaf67a0f3
errphoenix <herrphoenix20@gmail.com>
2026-01-26 01:05:03 +0100

    Create float abstraction
    This is primarily because it might be necessary to switch to 64 bits floating points in the future, in the case the precision of 32 bits is not enough for the physical simulation

--- src/state/mod.rs
+++ src/state/mod.rs
@@ -1,10 +1,52 @@
+use std::{
+    fmt::Display,
+    ops::{Deref, DerefMut},
+    time::Duration,
+};
+
 use crate::{mesh, state::column::Column};
 
 pub mod column;
 pub mod cross;
 
-// in case a switch to higher precision with f64 is required
-pub(crate) type Real = f32;
+#[derive(Clone, Copy, PartialEq, PartialOrd, Default, Debug)]
+pub struct Real(FloatType);
+
+type FloatType = f32;
+
+impl Display for Real {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl Deref for Real {
+    type Target = FloatType;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for Real {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl Real {
+    pub fn new(float: FloatType) -> Self {
+        Self(float)
+    }
+
+    pub fn as_f32(&self) -> f32 {
+        self.0 as f32
+    }
+
+    pub fn as_f64(&self) -> f64 {
+        self.0 as f64
+    }
+}
 
 // X, Y, Z
 type Position = [Real; 3];
