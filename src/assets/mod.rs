4856ad0176e6e5e58d94f10a45823c90be213939
errphoenix <herrphoenix20@gmail.com>
2026-06-08 03:34:06 +0200

    assets: move away from per-asset state machine to direct presence checks

--- src/assets/mod.rs
+++ src/assets/mod.rs
@@ -112,31 +112,19 @@ pub trait AsView {
     fn as_view(&self) -> Self::View;
 }
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Default)]
-pub enum ResourceState {
-    #[default]
-    Unloaded,
-    InMemory,
-    Processed,
-}
-
-impl std::fmt::Display for ResourceState {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            ResourceState::Unloaded => write!(f, "UNLOADED"),
-            ResourceState::InMemory => write!(f, "IN RAM"),
-            ResourceState::Processed => write!(f, "PROCESSED (GPU)"),
-        }
-    }
-}
-
 #[derive(Debug, thiserror::Error)]
 pub enum AssetError {
-    #[error("invalid resource state for requested operation: expected {expected}, got {present}.")]
-    InvalidState {
-        present: ResourceState,
-        expected: ResourceState,
-    },
+    #[error("resource is not present in video memory")]
+    NotProcessed,
+
+    #[error("failed to process resource for gpu: already processed")]
+    AlreadyProcessed,
+
+    #[error("resource is not present in memory")]
+    NotInMemory,
+
+    #[error("failed to load resource: already loaded in memory")]
+    AlreadyInMemory,
 
     #[error("failed to load: file not found at path {0}")]
     FileNotFound(PathBuf),
@@ -164,21 +152,9 @@ pub struct Handle<T>
 where
     T: Import + Upload,
 {
-    state: ResourceState,
     source: PathBuf,
     raw_resource: Option<T>,
     gpu_resource: Option<T::AsGpu>,
-}
-
-macro_rules! assert_state {
-    ($s:ident, $state:expr) => {
-        if $s.state != $state {
-            return Err(AssetError::InvalidState {
-                present: $s.state,
-                expected: $state,
-            });
-        }
-    };
 }
 
 impl<T> Handle<T>
@@ -200,14 +176,13 @@ where
     pub fn new<P: AsRef<Path>>(path: P) -> Self {
         Self {
             source: path.as_ref().to_path_buf(),
-            state: ResourceState::Unloaded,
             raw_resource: None,
             gpu_resource: None,
         }
     }
 
-    pub const fn state(&self) -> ResourceState {
-        self.state
+    pub fn is_unloaded(&self) -> bool {
+        self.raw_resource.is_none() && self.gpu_resource.is_none()
     }
 
     pub const fn is_in_memory(&self) -> bool {
@@ -228,27 +203,6 @@ where
 
     pub fn gpu_resource(&self) -> Option<&T::AsGpu> {
         self.gpu_resource.as_ref()
-    }
-
-    /// Invalidates the asset's memory state to [`ResourceState::Unloaded`].
-    ///
-    /// This allows the asset's resource to be reloaded or replaced with
-    /// [`Self::load_to_memory`] and, if needed, [`Self::upload_to_gpu`] later.
-    ///
-    /// This can be useful if the data has changed on the disk, and needs to be
-    /// updated.
-    pub fn invalidate_disk(&mut self) {
-        self.state = ResourceState::Unloaded;
-    }
-
-    /// Invalidates the asset's gpu state to [`ResourceState::InMemory`].
-    ///
-    /// This allows the asset's resource to be reloaded or replaced with
-    /// [`Self::upload_to_gpu`] later.
-    /// This can be useful if the data has changed in memory, and needs to be
-    /// updated on the gpu.
-    pub fn invalidate_gpu(&mut self) {
-        self.state = ResourceState::InMemory;
     }
 
     /// Attempt to load the raw resource from disk.
@@ -277,7 +231,7 @@ where
     /// [`AssetError::FileImageLoadError`]: FileImageLoadError
     pub fn load_to_memory(&mut self) -> AssetResult<&T> {
         if self.raw_resource.is_some() {
-            assert_state!(self, ResourceState::Unloaded);
+            return Err(AssetError::AlreadyInMemory);
         }
 
         let path = &self.source;
@@ -286,10 +240,7 @@ where
         }
 
         let loaded = T::from_file(path)?;
-
         self.raw_resource = Some(loaded);
-        self.state = ResourceState::InMemory;
-
         Ok(self.raw_resource.as_ref().unwrap())
     }
 
@@ -303,7 +254,9 @@ where
     /// [`AssetError::NoGlContext`] is returned if the GL context is
     /// unavailable on this thread.
     pub fn upload_to_gpu(&mut self) -> AssetResult<&T::AsGpu> {
-        assert_state!(self, ResourceState::InMemory);
+        if self.raw_resource.is_none() {
+            return Err(AssetError::NotInMemory);
+        }
 
         if !janus::gl::has_gl_init() {
             return Err(AssetError::NoGlContext);
@@ -311,10 +264,7 @@ where
 
         let raw_resource = self.raw_resource.as_ref().unwrap();
         let gpu_resource = raw_resource.upload_to_gpu()?;
-
         self.gpu_resource = Some(gpu_resource);
-        self.state = ResourceState::Processed;
-
         Ok(self.gpu_resource.as_ref().unwrap())
     }
 
@@ -336,21 +286,15 @@ where
     /// [`AssetError::NoGlContext`] is returned if the GL context is
     /// unavailable on this thread.
     pub fn free_from_gpu(&mut self) -> AssetResult<()> {
-        assert_state!(self, ResourceState::InMemory);
-
+        if self.gpu_resource.is_none() {
+            return Err(AssetError::NotProcessed);
+        }
         if !janus::gl::has_gl_init() {
             return Err(AssetError::NoGlContext);
         }
 
         let gpu_resource = self.gpu_resource.take();
         drop(gpu_resource);
-
-        if self.raw_resource.is_some() {
-            self.state = ResourceState::InMemory;
-        } else {
-            self.state = ResourceState::Unloaded;
-        }
-
         Ok(())
     }
 
@@ -360,14 +304,10 @@ where
     /// [`ResourceState::Unloaded`] if it is not loaded on the gpu.
     pub fn take_from_memory(&mut self) -> AssetResult<T> {
         if self.raw_resource.is_none() {
-            assert_state!(self, ResourceState::InMemory);
+            return Err(AssetError::NotInMemory);
         }
 
         let raw_resource = self.raw_resource.take().unwrap();
-        if self.state == ResourceState::InMemory {
-            self.state = ResourceState::Unloaded;
-        }
-
         Ok(raw_resource)
     }
 
